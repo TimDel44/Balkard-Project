00298bf165d03bc5dfa4acfd9044aa26b8a7a4f8
TimDel <tim.del44210@gmail.com>
2020-03-26 12:10:34 +0100

    création de la class personnages et setup des getteurs et setteurs

---
+++ Balkard-Project/personnage.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class personnage
+{
+private :
+	std::string nom;
+	int vie;
+	int attaque;
+	int defense;
+	int perception;
+	int initiative;
+	int esquive;
+	int pa;
+
+
+
+public :
+	std::string getNom() { return this->nom; }
+	int getVie() { return this->vie; }
+	int getAttaque() { return this->attaque; }
+	int getDefense() { return this->defense; }
+	int getPerception() { return this->perception; }
+	int getInitiative() { return this->initiative; }
+	int getEsquive() { return this->esquive; }
+	int getPA() { return this->pa; }
+
+	std::string setNom(std::string nom) { this->nom = nom; }
+	int setVie(int vie) {this->vie = vie; }
+	int setAttaque(int attaque) {this->attaque = attaque; }
+	int setDefense(int defense) {this->defense = defense; }
+	int setPerception(int perception) {this->perception = perception; }
+	int setInitiative(int initiative) {this->initiative = initiative; }
+	int setEsquive(int esquive) {this->esquive = esquive; }
+	int setPA(int pa) {this->pa = pa; }
+
+};
+
