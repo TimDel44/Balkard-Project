64a2ad88a25837378dbb2f04eaaa01ab7caf6aa8
TimDel <tim.del44210@gmail.com>
2020-04-06 18:46:52 +0200

    ajout titre en ascii + typo

--- Balkard-Project/Balkard-Project.cpp
+++ Balkard-Project/Balkard-Project.cpp
@@ -24,6 +24,7 @@
 #include "carteArgent.h"
 #include "joueur.h"
 #include "Deck.h"
+#include "Titre.h"
 
 
 using namespace std;
@@ -34,7 +35,13 @@ int main()
 {
 
     srand(time(NULL));
-    cout << "Bienvenue sur Balkard...\n" << endl;
+    cout << " 888888ba  oo                                                                                                 888888ba           dP dP                               dP" << endl;
+    cout << " 88    `8b                                                                                                    88    `8b          88 88                               88" << endl;
+    cout << " 88aaaa8P' dP .d8888b. 88d888b. dP   .dP .d8888b. 88d888b. dP    dP .d8888b.    .d8888b. dP    dP 88d888b.    88aaaa8P' .d8888b. 88 88  .dP  .d8888b. 88d888b. .d888b88" << endl;
+    cout << " 88   `8b. 88 88ooood8 88'  `88 88   d8' 88ooood8 88'  `88 88    88 88ooood8    Y8ooooo. 88    88 88'  `88    88   `8b. 88'  `88 88 88888\"   88\'  `88 88'  `88 88\'  `88" << endl;
+    cout << " 88    .88 88 88.  ... 88    88 88 .88'  88.  ... 88    88 88.  .88 88.  ...          88 88.  .88 88          88    .88 88.  .88 88 88  `8b. 88.  .88 88       88.  .88" << endl;
+    cout << " 88888888P dP `88888P' dP    dP 8888P'   `88888P' dP    dP `88888P' `88888P'    `88888P' `88888P' dP          88888888P `88888P8 dP dP   `YP `88888P8 dP       `88888P8    88    88    88 "<< endl;
+    //new Titre();
     new Deck();
     string nom;
     cout << "Entrez le nom du joueur\n" << endl;
