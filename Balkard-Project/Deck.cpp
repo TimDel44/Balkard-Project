9a89f209ac9dcc9859fc8224189e4ce049710d7a
TimDel <tim.del44210@gmail.com>
2020-04-09 00:29:54 +0200

    +1

--- Balkard-Project/Deck.cpp
+++ Balkard-Project/Deck.cpp
@@ -56,22 +56,22 @@ Deck::Deck()
     for (a = 0; a < 3; a++) {
         b = (3 - a);
         for (nbdecarte = 0; nbdecarte < b; nbdecarte++) {
-             cartes.push_back(new carteArmure(objet[3], stat[2], materiauArmure[a], a, 2));
-             cartes.push_back(new carteBotte(objet[4], stat[2], materiauArmure[a], a, 2));
+             cartes.push_back(new carteArmure(objet[3], stat[2], materiauArmure[a], a+1, 2));
+             cartes.push_back(new carteBotte(objet[4], stat[2], materiauArmure[a], a+1, 2));
         }
     }
     //CARTE ARME
     for (a = 0; a < 3; a++) {
         b = (3 - a);
         for (nbdecarte = 0; nbdecarte < b; nbdecarte++) {
-            cartes.push_back(new carteArme(objet[5], stat[1], materiauArme[a], a, 1));
+            cartes.push_back(new carteArme(objet[5], stat[1], materiauArme[a], a+1, 1));
         }
     }
     //CARTE TALISMAN
     for (a = 0; a < 3; a++) {
         b = (3 - a);
         for (nbdecarte = 0; nbdecarte < b; nbdecarte++) {
-            cartes.push_back(new carteTalisman(objet[6], "PA", niveau[a], a, 6));
+            cartes.push_back(new carteTalisman(objet[6], "PA", niveau[a], a+1, 6));
         }
     }
     /*
